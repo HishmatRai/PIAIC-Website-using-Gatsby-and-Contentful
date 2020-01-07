d5e210c179d711532764fe3f06d9cf9e84c5af76
HishmatRai <hishmatrai.2018@gmail.com>
2020-01-07 20:19:27 +0500

    nodejs path correction

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,7 +23,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogTemplate = path.resolve("./src/templetes/courses.js")
+  const blogTemplate = path.resolve("src/templetes/courses.js")
   const res = await graphql(`
     query {
       allContentfulCourses {
